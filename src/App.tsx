30b368aa7f2ab081b2d552271af8d5eebe1d8d9e
Alert1437 <a.a.cherkaev@gmail.com>
2024-04-10 13:05:50 +0300

    change: Remove traces

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {Home} from "./screens/Home.tsx";
 import {Connect} from "./screens/Connect.tsx";
 import {Viewport} from "./shared/ui/Viewport/Viewport.tsx";
 import {ThemeProvider} from "./features/theme/ThemeProvider.tsx";
-import {Try} from "./screens/Try.tsx";
 import {FailureView} from "./screens/FailureView.tsx";
 import {TryCallback} from "./screens/TryCallback.tsx";
 
@@ -17,7 +16,6 @@ const App = () => {
                         <Route path="/" element={<Home />} />
                         <Route path="/connect" element={<Connect />} />
                         <Route path="/failure" element={<FailureView />} />
-                        <Route path="/try" element={<Try />} />
                         <Route path="/callback" element={<TryCallback />} />
                     </Routes>
                 </BrowserRouter>
