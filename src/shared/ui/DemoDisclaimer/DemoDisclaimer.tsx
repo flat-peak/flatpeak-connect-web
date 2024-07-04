460d888c5644dc95092af4fd7e0dfd3739a502d1
alenb <alenberg@gmail.com>
2024-07-04 08:51:00 +0100

    Shorten demo message to fit single line

--- src/shared/ui/DemoDisclaimer/DemoDisclaimer.tsx
+++ src/shared/ui/DemoDisclaimer/DemoDisclaimer.tsx
@@ -4,16 +4,19 @@ import Typography from "../Typography/Typography.tsx";
 const DISPLAY_DEMO_DISCLAIMER = true;
 
 export default function DemoDisclaimer() {
-  return DISPLAY_DEMO_DISCLAIMER && (
-      <WarningMessage color={"grey"}>
+  return (
+    DISPLAY_DEMO_DISCLAIMER && (
+      <WarningMessage color={'grey'}>
         <div>
-          <Typography variant={"rp_300_14"} color={"black"}>
-            Hosted Connect-web demo. Not for customer use. To remove, see{" "}
-            <a href={"https://docs.flatpeak.com/guides/integration-test"} target={"_blank"}>
-              <Typography variant={"rp_300_14"} color={"black"} decoration={"underline"} component={"span"}>Docs.</Typography>
-            </a>.
+          <Typography variant={'rp_300_14'} color={'black'}>
+            Demo, not for live use.{' '}
+            <a href={'https://docs.flatpeak.com/guides/integration-test'} target={'_blank'}>
+              <Typography variant={'rp_300_14'} color={'black'} decoration={'underline'} component={'span'}>Docs to remove</Typography>
+            </a>
+            .
           </Typography>
         </div>
       </WarningMessage>
+    )
   )
 }
