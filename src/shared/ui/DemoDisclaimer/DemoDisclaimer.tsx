00de43eb79e5ee61069123e444c6891d7dd6c171
Alert1437 <a.a.cherkaev@gmail.com>
2024-07-04 21:49:39 +0300

    change: increase font

--- src/shared/ui/DemoDisclaimer/DemoDisclaimer.tsx
+++ src/shared/ui/DemoDisclaimer/DemoDisclaimer.tsx
@@ -8,10 +8,10 @@ export default function DemoDisclaimer() {
   return DISPLAY_DEMO_DISCLAIMER && (
         <div className={styles.host}>
           <WarningIcon color={'black'} opacity={1} width={16} height={16}/>
-          <Typography variant={"rp_300_11"}>
+          <Typography variant={"rp_300_14"}>
              Demo, not for live use.{" "}
             <a href={"https://docs.flatpeak.com/guides/integration-test"} target={"_blank"}>
-              <Typography variant={"rp_300_11"} decoration={"underline"} component={"span"}>Docs to remove</Typography>
+              <Typography variant={"rp_300_14"} decoration={"underline"} component={"span"}>Docs to remove</Typography>
             </a>.
           </Typography>
         </div>
