5eebaf977334e09c504759f6337be49fda10ce5b
Alert1437 <a.a.cherkaev@gmail.com>
2024-11-12 10:39:00 +0200

    fix: resolve typing issue

--- src/shared/TouchahbleOpacity/TouchableOpacity.tsx
+++ src/shared/TouchahbleOpacity/TouchableOpacity.tsx
@@ -2,13 +2,13 @@ import {ButtonHTMLAttributes, PropsWithChildren} from "react";
 import styles from "./TouchableOpacity.module.scss";
 
 type TouchableOpacityProps = {
-	component?: string | unknown;
+	component?: React.ElementType;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function TouchableOpacity(props: PropsWithChildren<TouchableOpacityProps>) {
 	const {children, className, component, ...buttonProps} = props;
 	const Tag = component || 'button';
-	if(Tag === 'button' && !buttonProps.type) {
+	if (Tag === 'button' && !buttonProps.type) {
 		buttonProps.type = "button"
 	}
 	return (
