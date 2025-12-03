6ee2e16af1e989608bf41262ad4fceaf4792f57b
Keigo Kudo <keigo.kudo@flatpeak.com>
2025-12-03 12:12:46 +0000

    feat: PRD-922: add filter function and text when there's no result

--- src/shared/ui/Combobox/Combobox.tsx
+++ src/shared/ui/Combobox/Combobox.tsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from "react";
+import { ChangeEventHandler, useMemo, useRef, useState } from "react";
 import styles from "./Combobox.module.scss";
 import Box from "../Box/Box";
 import SmallArrowRightIcon from "../icons/SmallArrowRightIcon";
+import Typography from "../Typography/Typography";
 
 export type ComboboxOption = { label: string; value: string };
 
@@ -10,11 +11,20 @@ export type ComboboxProps = {
   onChange?: (value: string | undefined) => void;
   name?: string;
   label?: string;
+  placeholder?: string;
+  noOptionsText?: string;
   className?: string;
 };
 
 export default function Combobox(props: ComboboxProps) {
-  const { options, onChange, name, label, className } = props;
+  const {
+    options,
+    onChange,
+    name,
+    placeholder = "Select an option",
+    noOptionsText = "No options found",
+    className,
+  } = props;
 
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState<string | undefined>(
@@ -41,6 +51,28 @@ export default function Combobox(props: ComboboxProps) {
     inputRef.current?.focus();
   };
 
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const nextValue = event.target.value;
+    setInputValue(nextValue);
+    // Open when it is typed
+    setIsOpen(true);
+  };
+
+  const filteredOptions = useMemo(() => {
+    const query = inputValue.trim().toLowerCase();
+    // return all options if query is empty
+    if (!query) return options;
+
+    const filteredOptions = options.filter(({ label, value }) => {
+      const labelText = label.toLowerCase();
+      const valueText = value.toLowerCase();
+      // return even if it is a partial match
+      return labelText.includes(query) || valueText.includes(query);
+    });
+
+    return filteredOptions;
+  }, [inputValue, options]);
+
   return (
     <Box rg={8}>
       <div ref={containerRef} className={`${styles.host}`}>
@@ -50,7 +82,8 @@ export default function Combobox(props: ComboboxProps) {
           type="text"
           value={inputValue}
           onClick={openList}
-          readOnly
+          onChange={handleInputChange}
+          placeholder={placeholder}
         />
 
         <div
@@ -64,7 +97,7 @@ export default function Combobox(props: ComboboxProps) {
         {/* select options come here */}
         {isOpen && (
           <div className={`${styles.popover}`}>
-            {options.map((option) => {
+            {filteredOptions.map((option) => {
               const { label, value } = option;
               const isSelected = value === selectedValue;
               return (
@@ -81,11 +114,19 @@ export default function Combobox(props: ComboboxProps) {
                 </button>
               );
             })}
+
+            {!filteredOptions.length && (
+              <div className={styles.noOptions}>
+                <Typography color="black_a40" variant="button__forms16_book">
+                  {noOptionsText}
+                </Typography>
+              </div>
+            )}
           </div>
         )}
 
         {/* hidden input to store the selected value */}
-        <input type="hidden" name={name} value={selectedValue || ""} />
+        <input type="hidden" name={name} value={inputValue || ""} />
       </div>
     </Box>
   );
